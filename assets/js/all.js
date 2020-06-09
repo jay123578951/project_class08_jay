b32e6c70bc888584fb2184a7961d079d0aac9ddf
Jay <jayxxx647@gmail.com>
2020-06-10 02:14:56 +0800

    Update 2020-06-09T18:14:06.226Z

--- assets/js/all.js
+++ assets/js/all.js
@@ -1,25 +1,25 @@
-$(document).ready(() => {
+"use strict";
+
+$(document).ready(function () {
   console.log('Hello Bootstrap4');
-});
+}); // 漢堡
 
-
-// 漢堡
 function openNav() {
   document.getElementById("mySidepanel").style.width = "100%";
 }
+
 function closeNav() {
   document.getElementById("mySidepanel").style.width = "0";
-}
+} // 輪播
 
-// 輪播
+
 $(document).ready(function () {
   var swiper = new Swiper('.swiper-container__banner', {
     navigation: {
       nextEl: '.banner__swiper-button-next',
-      prevEl: '.banner__swiper-button-prev',
-    },
+      prevEl: '.banner__swiper-button-prev'
+    }
   });
-
   var swiper = new Swiper('.topChoices__swiper-container', {
     //slidesPerView: 1,
     spaceBetween: 30,
@@ -28,20 +28,19 @@ $(document).ready(function () {
     // loopFillGroupWithBlank: true,
     breakpoints: {
       768: {
-        width: 270,
-        //slidesPerView: 1,
+        width: 270 //slidesPerView: 1,
+
       },
       992: {
-        width: 255,
-        //slidesPerView: 4,
+        width: 255 //slidesPerView: 4,
+
       }
     },
     navigation: {
       nextEl: '.topChoise__swiper-button-next',
-      prevEl: '.topChoise__swiper-button-prev',
-    },
+      prevEl: '.topChoise__swiper-button-prev'
+    }
   });
-
   var swiper = new Swiper('.getInspiration__swiper-container', {
     //slidesPerView: 1,
     spaceBetween: 30,
@@ -50,23 +49,23 @@ $(document).ready(function () {
     // loopFillGroupWithBlank: true,
     breakpoints: {
       768: {
-        width: 210,
-        //slidesPerView: 2,
+        width: 210 //slidesPerView: 2,
+
       },
       992: {
-        width: 350,
-        //slidesPerView: 3,
+        width: 350 //slidesPerView: 3,
+
       }
-    },
+    }
   });
 });
-
-$(document).ready(function() {
+$(document).ready(function () {
   rwd_fun();
   $(window).resize(rwd_fun);
 
   function rwd_fun() {
     var width = window.innerWidth;
+
     if (width < 768) {
       $('#filter__modalBody').append($('#filter__content'));
     } else {
@@ -74,3 +73,4 @@ $(document).ready(function() {
     }
   }
 });
+//# sourceMappingURL=all.js.map
